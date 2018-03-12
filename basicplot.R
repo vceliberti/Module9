5c4000c87969bef6edddb40d1f6e39f9a7ea7f5b
victoria <Owner@victoria.local>
2018-03-11 20:12:05 -0500

    basic plot

---
+++ basicplot.R
@@ -0,0 +1,7 @@
+plot(yearlytempNewHaven)  ##seeing what the default is
+
+plot(nhtemp)  ##seeing what default is
+plot(nhtemp, main = "Yearly Temperature in New Haven", xlab = "Year", ylab = "Temperature in Fahrenheit",
+     type = "b", las = 1, col = "sienna", col.main = "darkcyan", lty = "longdash",
+     bty = "n", bg = "transparent")
+colors()
